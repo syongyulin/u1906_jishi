4700a1f69f57a826e061c1201499c8583c2d3759
syl <1030918477@qq.com>
2020-09-07 11:41:42 +0800

    新建dev(dev)

--- u1906_jishi/urls.py
+++ u1906_jishi/urls.py
@@ -18,5 +18,6 @@ from django.urls import path
 from book import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.index)
+    path('',views.index),
+    path('dev/',views.dev)
 ]
